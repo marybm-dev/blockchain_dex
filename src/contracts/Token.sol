43872870d52fce160999b5826cf14371b3910aa6
Mary <mlmartinez85@gmail.com>
2022-01-16 12:20:36 -0800

    implement token transfers

--- src/contracts/Token.sol
+++ src/contracts/Token.sol
@@ -1,12 +1,30 @@
 pragma solidity^0.5.16;
+import "openzeppelin-solidity/contracts/math/SafeMath.sol";
 
 contract Token {
+    using SafeMath for uint;
+
+    // variables
     string public name = "Queer Token";
     string public symbol = "QUEER";
     uint256 public decimals = 18;
     uint256 public totalSupply;
+    mapping(address => uint256) public balanceOf;
+
+    // events
+    event Transfer(address indexed from, address indexed to, uint256 value);
 
     constructor() public {
         totalSupply = 69420 * (10 ** decimals);
+        balanceOf[msg.sender] = totalSupply;
+    }
+
+    function transfer(address _to, uint256 _value) public returns (bool success) {
+        require(_to != address(0));
+        require(balanceOf[msg.sender] >= _value);
+        balanceOf[msg.sender] = balanceOf[msg.sender].sub(_value);
+        balanceOf[_to] = balanceOf[_to].add(_value);
+        emit Transfer(msg.sender, _to, _value);
+        return true;
     }
 }
--- test/Token.test.js
+++ test/Token.test.js
@@ -1,15 +1,17 @@
+import { tokens, EVM_REVERT } from './helpers'
+
 const Token = artifacts.require('./Token')
 
 require('chai')
     .use(require('chai-as-promised'))
     .should()
 
-contract('Token', (accounts) => {
+contract('Token', ([deployer, receiver]) => {
     const name = 'Queer Token'
     const symbol = 'QUEER'
     const decimals = '18'
-    const totalSupply = '69420000000000000000000'
-    
+    const totalSupply = tokens(69420).toString()
+
     let token
 
     beforeEach(async () => {
@@ -37,7 +39,61 @@ contract('Token', (accounts) => {
 
         it('tracks the total supply', async() => {
             const result = await token.totalSupply()
-            result.toString().should.equal(totalSupply)
+            result.toString().should.equal(totalSupply.toString())
+        })
+
+        it('assings the totaly supply of deployer', async() => {
+            const result = await token.balanceOf(deployer)
+            result.toString().should.equal(totalSupply.toString())
+        })
+    })
+
+    describe('sending tokens', () => {
+        let amount
+        let result
+
+        describe('success', async () => {
+            beforeEach(async () => {
+                amount = tokens(10)
+                result = await token.transfer(receiver, amount, { from: deployer })
+            })
+
+            it('transfer token balances', async() => {
+                let balanceOf
+                balanceOf = await token.balanceOf(deployer)
+                balanceOf.toString().should.equal(tokens(69410).toString())
+                balanceOf = await token.balanceOf(receiver)
+                balanceOf.toString().should.equal(tokens(10).toString())
+            })
+
+            it('emits a transfer event', async() => {
+                const log = result.logs[0]
+                log.event.should.eq('Transfer')
+                const event = log.args
+                event.from.toString().should.equal(deployer, 'from is correct')
+                event.to.should.equal(receiver, 'to is correct')
+                event.value.toString().should.equal(amount.toString(), 'value is correct')
+            })
+        })
+
+        describe('failure', async () => {
+            beforeEach(async () => {
+
+            })
+
+            it('rejects insufficient balances', async() => {
+                let invalidAmount
+                invalidAmount = tokens(100000) // 100k > totalSupply
+                await token.transfer(receiver, invalidAmount, { from: deployer }).should.be.rejectedWith(EVM_REVERT);
+
+                // attempt token transfers when you have none
+                invalidAmount = tokens(10)
+                await token.transfer(deployer, invalidAmount, { from: receiver }).should.be.rejectedWith(EVM_REVERT);
+            })
+
+            it('rejects invalid recipients', async() => {
+                await token.transfer(0x0, amount, { from: deployer }).should.be.rejected;
+            })
         })
     })
 })
